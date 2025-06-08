b2c1faa5d8e7b35917e74dda5c3e004ed6553176
miguelmagbitang <magbitangmiguel@gmail.com>
2025-06-08 13:31:20 +0800

    Use H2 database for unit tests

--- src/test/java/com/muggedbits/rewardz/RewardzApplicationTests.java
+++ src/test/java/com/muggedbits/rewardz/RewardzApplicationTests.java
@@ -2,8 +2,10 @@ package com.muggedbits.rewardz;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RewardzApplicationTests {
 
 	@Test
